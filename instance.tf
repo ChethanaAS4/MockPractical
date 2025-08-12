7a112fcce0bf2c0299da7777da7ea782fb643bf1
ChethanaAS4 <chethanakanadmane@gmail.com>
2025-08-12 15:13:05 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,3 +1,22 @@
+resource "tls_private_key" "newkeys" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.newkeys.private_key_pem
+  filename = "${path.module}/mock-key.pem"
+  file_permission = "0400"
+}
+
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "mock-terraform-key"
+  public_key = tls_private_key.newkeys.public_key_openssh
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -14,9 +33,10 @@ resource "aws_instance" "MockInstance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
   subnet_id = aws_subnet.public-subnet-1.id
-  key_name      = "Mockkey"
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  key_name      = aws_key_pair.key_pair.key_name
 
   tags = {
     Name = "MockInstance"
@@ -32,13 +52,13 @@ provisioner "remote-exec" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("C:/Users/bubbl/Videos/Mockkey.pem")
+      private_key = tls_private_key.newkeys.private_key_pem
       host        = self.public_ip
     }
   }
 }
 
-resource "aws_eip" "lb" {
+resource "aws_eip" "eip" {
   instance = aws_instance.MockInstance.id
   domain   = "vpc"
 }
